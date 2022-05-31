00c826af5992b90bf0115f40b7bf5cdd39ca1009
UserHab <vitalibembel1.21@gmail.com>
2022-05-31 15:10:51 +0300

    comm mes

--- src/main/java/com/company/Query/Query9.java
+++ src/main/java/com/company/Query/Query9.java
@@ -1,2 +1,26 @@
-package com.company.Query;public class Query9 {
+package com.company.Query;
+
+import com.company.Connection.JDBC;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Query9 {
+    public static void main(String[] args) {
+        Statement stmt = null;
+        try {
+
+            // Open a connection
+            JDBC.connect();
+
+            // Execute a query
+            stmt = JDBC.connection.createStatement();
+            int rows = stmt.executeUpdate("DELETE FROM AUTHORS WHERE firstName = 'Vova'");
+            System.out.printf(" %d row(s) is deleted", rows);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
